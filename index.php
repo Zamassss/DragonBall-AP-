49061f6c000a1437718e133c7e7b17afdb4ec41e
Zamassss <38056063+Zamassss@users.noreply.github.com>
2018-04-04 02:01:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 
+<?php
+
+$api = "bot<585920219:AAFBiNHtyB-J-JGibUxD_8es7ZtO2tgsXyk>"; //al posto di <TOKEN> inserite il token del bot
+
+$input = file_get_contents("php://input");
+$update = json_decode($input, true);
+
+$message = $update['message']['text'];
+$chatid = $update['message']['chat']['id'];
+
+function sendMessage($chatid, $text)
+{
+	global $api;	$url = "https://api.telegram.org/$api/sendMessage?chat_id=".$chatid."&text=".urlencode($text);
+	$get = file_get_contents($url);
+}
+
+if($message == "/start")
+{
+	sendMessage($chatid, "Strunz");
+}
+
+?>
